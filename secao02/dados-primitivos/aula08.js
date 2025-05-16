4e0eb98461f32a525b15fb52b3e7a9ba6989950e
Mário César <mariocesarmoliveirafilho@gmail.com>
2025-05-16 17:30:58 -0300

    dados-primitivos

---
+++ secao02/dados-primitivos/aula08.js
@@ -0,0 +1,10 @@
+const nome1 = "Luiz"; //String
+const nome2 = 'Luiz'; //String
+const nome3 = `Luiz`; //String
+const numero1 = 10; //Number
+const numero2 = 10.5; //Number
+let nomeAluno; //Não está alocado em luga nenhum
+const sobreNomeAluno = null; //Nul -> não está alocado em nenhum lugar
+const aprovado = true// Boolean - somente verdadeiro ou falso
+
+//typeof -> ver o tipo da variavel 
