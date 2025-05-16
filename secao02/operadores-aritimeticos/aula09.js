aba5dba1be320539c777b168f0b16a0e6b51087f
Mário César <mariocesarmoliveirafilho@gmail.com>
2025-05-16 17:48:13 -0300

    operadores-aritimeticos

---
+++ secao02/operadores-aritimeticos/aula09.js
@@ -0,0 +1,11 @@
+const n1 = 5;
+const n2 = 10;
+
+//adiçao +
+//subtração -
+//divisão
+//multiplicação *
+//potenciação **
+//resto da divisão %
+//contador++ -> primeiro ele mostra, depois ele incrementa
+//++contador -> primeiro ele incrementa e depois ele mostra
