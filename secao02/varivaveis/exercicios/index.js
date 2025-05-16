c1034a7798ed268b66e7cc2407be414e8db8fbd3
Mário César <mariocesarmoliveirafilho@gmail.com>
2025-05-16 14:57:27 -0300

    exercicio imc

---
+++ secao02/varivaveis/exercicios/index.js
@@ -0,0 +1,8 @@
+const nome = "Lucas";
+const sobrenome = "Almeida";
+const idade = 40;
+const peso = 68;
+const altura = 1.75
+
+const imc = peso / (altura*altura);
+console.log(nome+" "+sobrenome+" tem "+idade+" anos, pesa "+peso+" kg tem "+altura+" de altura e seu IMC é de "+imc);
