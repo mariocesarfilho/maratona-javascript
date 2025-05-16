cf50f283376e832ebb758e358728d1fba2b89b83
Mário César <mariocesarmoliveirafilho@gmail.com>
2025-05-16 10:21:35 -0300

    aula 03

---
+++ aula03/index.js
@@ -0,0 +1,6 @@
+console.log('Hello, World!'); //String
+console.log("Hello, World!");
+console.log(`Hello, World!`);
+
+console.log(123456); //Número literal
+console.log(12.5); //Número de Ponto Flutuante
