af7c58096264de482f35826cbb512ee921dbcf7f
Mário César <mariocesarmoliveirafilho@gmail.com>
2025-05-20 15:44:08 -0300

    aula12 - object math

---
+++ secao02/aula12 - object math/index.js
@@ -0,0 +1,11 @@
+let num1 = 9.50489742;
+
+let auxBaixo = Math.floor(num1); //Arredonda para baixo
+let auxCima = Math.ceil(num1); //Arredonda para cima
+let auxArredonda = Math.round(num1); //Arredonda, até 04 para baixo depois para cima
+// console.log(Math.max(1.3,134,3.4,-1)); Pega o maior número da sequência
+
+const aletatorio = Math.random() * (10 - 5) + 5;//Pega um número aleatório entre 10 e
+// console.log(Math.random()); //Gera um número aleatório entre 0 e 1 e o 1 nunca é incluido
+
+console.log(aletatorio);
