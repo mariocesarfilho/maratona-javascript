1d556ba845c67325b5b36749bf247cf24ef7c10c
Mário César <mariocesarmoliveirafilho@gmail.com>
2025-05-21 17:29:17 -0300

    aula14 - funções(basico)

---
+++ secao02/aula14 - funções(basico)/index.js
@@ -0,0 +1,12 @@
+// function soma(a,b){
+//     return a + b; //tudo o que está abaixo de return não é executado
+// }
+
+// console.log(soma(2,2));
+
+// Função Anônima -> Jogar uma função dentro de uma variável
+const raiz = (n) => {
+    return Math.sqrt(n);
+};
+
+console.log(raiz(9));
