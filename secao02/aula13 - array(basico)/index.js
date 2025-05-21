8ae7264de8a15f1992e00a3e0836691c254b0451
Mário César <mariocesarmoliveirafilho@gmail.com>
2025-05-21 16:58:58 -0300

    aula13 - array(basico)

---
+++ secao02/aula13 - array(basico)/index.js
@@ -0,0 +1,18 @@
+const alunos = ['Lucas', 'Mario', 'João', ];
+
+//Faz um push na unha
+// alunos[alunos.length] = 'Luiza'; 
+// alunos[alunos.length] = 'Paulo';
+//Push - Adiciona no final da lista
+// alunos.push('Paulo', 'Luiza');
+
+// alunos.unshift('Paula'); //Adiciona no começo
+
+//Eu consigo guardar o objeto que eu estou removendo
+// const alunoRemovido = alunos.pop(); //Remove do final do array
+
+// alunos.shift(); //Remove no começo
+
+console.log(alunos);
+
+delete alunos[1]; //Removo um objeto pelo indice no array
