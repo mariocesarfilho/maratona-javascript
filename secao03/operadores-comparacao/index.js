ebb107a923daefeb27b1e971796cabb25ae6e689
Mário César <mariocesarmoliveirafilho@gmail.com>
2025-05-26 08:29:21 -0300

    operadores logicos

---
+++ secao03/operadores-comparacao/index.js
@@ -0,0 +1,11 @@
+/**
+ * Operadores de Comparação
+ * > maior que
+ * < menor que
+ * >= maior igual a 
+ * <= menor igual a
+ * == igualdade 
+ * === igualdade restrita
+ * != diferente
+ * !== diferente restrito
+ */
